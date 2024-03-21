b61683c1299d56797d5fd10b63d970f89bb73d06
Calvin Raab <37590049+JaclynW@users.noreply.github.com>
2024-03-20 17:24:35 -0700

    Homepage halfway styled

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add functionality to process the search form
+    alert('Search functionality to be implemented');
+});
