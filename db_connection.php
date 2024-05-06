65988754f870e15602be76f0f2a7e3f2c22715c3
Benno <99782581+Scizmz@users.noreply.github.com>
2024-05-06 16:44:44 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";  // the server where the database is hosted
-$username = "your_username";  // the username to log into the database
-$password = "your_password";  // the password to log into the database
-$dbname = "your_dbname";  // the name of the database
+$username = "team_3";  // the username to log into the database
+$password = "zf7vf2z0";  // the password to log into the database
+$dbname = "team_3";  // the name of the database
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
