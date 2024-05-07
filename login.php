a87d1e7c1c5ce6ec8313345f67af5cbfcb88d320
Benno <99782581+Scizmz@users.noreply.github.com>
2024-05-07 11:52:32 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Study Spot Finder - Login</title>
+   <link rel="icon" href="assets/icon.png" type="image/png">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+   <nav id="navbar">
+       <div id="logo"><a href="index.html">Study Spot Finder</a> > Login</div>
+   </nav>
+
+   <div class="section" id="registration-section">
+       <div class="loginContent">
+           <h1>Login</h1>
+           <p>Create an account to find and save your study spots.</p>
+
+           <form id="registration-form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+               <label for="username">Username</label><br />
+               <input type="text" id="Username" name="username" required><br><br>
+               <label for="password">Password</label><br />
+               <input type="password" id="password" name="password" required><br /><br>
+               <button class="loginButton" type="submit">Login</button>
+           </form><br><br>
+
+           <p>If you don't have an account, you can easily register one!</p>
+           <button class="loginButton" onclick="window.location.href='register.php';">Register</button>
+       </div>
+   </div>
+
+   <footer>
+       <p>
+           <a href="contact.html">Contact Us</a> | 
+           <a href="sitemap.html">Site Map</a> |
+           <a href="about.html">About Us</a>
+       </p>
+   </footer>
+
 <?php
 // Database credentials 
 $servername = "localhost";
@@ -62,12 +102,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<?php if (isset($errorMessage)) : ?>
-    <div class="error-message"><?= $errorMessage ?></div>
-<?php endif; ?>
 
 </body>
 </html>
